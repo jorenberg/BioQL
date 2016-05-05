a6c21ee28c6daf2eaeb76a106bcb9ddb56ee5b78
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-05 17:04:03 +0530

    Initial commit
    Using use - Imports some semantics into the current package from the named module, generally by aliasing certain subroutine or variable names into the package. PERL - v5.18.2, http://www.perl.org/.
    Usage: $ perl -v

--- script/analysis.pl
+++ script/analysis.pl
@@ -5,3 +5,5 @@
 # CB2L®     : Computational Biology and Bioinformatics Laboratory.
 # Author    : Prabhat Kumar [http://prabhatkumar.org/].
 # Copyright : Sequømics Corporation [http://sequomics.com/].
+
+use 5.18.2;
