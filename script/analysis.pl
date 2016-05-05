789190309cf28e77a7d417b3c0d10ea23fba4ad6
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-05 16:58:09 +0530

    Initial commit
    @iamprabhat, Prabhat Kumar, http://prabhatkumar.org/.

--- script/analysis.pl
+++ script/analysis.pl
@@ -1,1 +1,7 @@
 #!/usr/bin/perl
+
+
+# BioQL®    : An efficient Query Language for interpreting genome and proteome.
+# CB2L®     : Computational Biology and Bioinformatics Laboratory.
+# Author    : Prabhat Kumar [http://prabhatkumar.org/].
+# Copyright : Sequømics Corporation [http://sequomics.com/].
