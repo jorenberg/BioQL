25b5bf73cc4ee35d149030f64ae67430bb783eae
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-06 14:35:09 +0530

    Initial commit
    Synopsis - A brief summary or general survey of Analysis PERL program.
    @iamprabhat - Prabhat Kumar, http://prabhatkumar.org/.

--- script/analysis.pl
+++ script/analysis.pl
@@ -13,3 +13,17 @@ use utf8;
 use File::Spec;
 
 use version; our $VERSION = version->parse("1.0.0");
+
+
+# -------- \\
+# Synopsis  \\
+# ----------------------------------------------------------------------------
+# 1. To read the DNA/RNA/Protein "FASTA" formatted sequence file,            |
+# 2. To clean the DNA/RNA/Protein sequence,                                  |
+# 3. To count DNA/RNA base composition and Protein's amino acid composition, |
+# 4. To determine DNA/RNA complements,                                       |
+# 5. To find all AT-rich k-(4-7)mers,                                        |
+# 6. To find all CG-rich k-(4-7)mers,                                        |
+# 7. To find Protein's properties,                                           |
+# 8. To store the result(s).                                                 |
+# ----------------------------------------------------------------------------
