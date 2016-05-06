6536755a583eb79a252dc976def3d8c2f592ff81
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-06 17:29:39 +0530

    Initial commit
    Synopsis - A brief summary of Concatenating DNA/RNA/Protein Fragments PERL program.
    @iamprabhat - Prabhat Kumar, http://prabhatkumar.org/.

--- script/concat.pl
+++ script/concat.pl
@@ -13,3 +13,13 @@ use utf8;
 use File::Spec;
 
 use version; our $VERSION = version->parse("1.0.0");
+
+
+# -------- \\
+# Synopsis  \\
+# ----------------------------------------------------------------------------
+# 1. To read the DNA/RNA/Protein "FASTA" formatted sequence file,            |
+# 2. To concatenating DNA/RNA Fragments,                                     |
+# 3. To concatenating Protein Fragments,                                     |
+# 4. To store the result(s).                                                 |
+# ----------------------------------------------------------------------------
