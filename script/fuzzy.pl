3a9bbe08c41260a7188adb4d8a57e7c21234ab65
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-07 12:52:49 +0530

    Initial commit
    Synopsis - A brief summary of Fuzzy Matching the DNA/RNA/Protein Fragments PERL program.
    @iamprabhat - Prabhat Kumar, http://prabhatkumar.org/.

--- script/fuzzy.pl
+++ script/fuzzy.pl
@@ -13,3 +13,13 @@ use utf8;
 use File::Spec;
 
 use version; our $VERSION = version->parse("1.0.0");
+
+
+# -------- \\
+# Synopsis  \\
+# ----------------------------------------------------------------------------
+# 1. To read the DNA/RNA/Protein "FASTA" formatted sequence file,            |
+# 2. To Fuzzy Matching the DNA/RNA Fragments,                                |
+# 3. To Fuzzy Matching the Protein Fragments,                                |
+# 4. To store the result(s).                                                 |
+# ----------------------------------------------------------------------------
